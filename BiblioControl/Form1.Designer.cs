2269d7a807c94598efb00e681a6ff8d4b12fa069
Andrey Nikitin <an636646@gmail.com>
2024-12-23 21:44:58 +0200

    Add UI to function issue book

--- BiblioControl/Form1.Designer.cs
+++ BiblioControl/Form1.Designer.cs
@@ -46,6 +46,11 @@
             this.readersListBox = new System.Windows.Forms.ListBox();
             this.booksListBox = new System.Windows.Forms.ListBox();
             this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.issueBookButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // registeredButton
@@ -198,11 +203,59 @@
             this.label10.TabIndex = 31;
             this.label10.Text = "Books";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(17, 219);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "Reader";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(17, 193);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 37;
+            this.label9.Text = "Book";
+            // 
+            // issueBookButton
+            // 
+            this.issueBookButton.Location = new System.Drawing.Point(104, 242);
+            this.issueBookButton.Name = "issueBookButton";
+            this.issueBookButton.Size = new System.Drawing.Size(75, 23);
+            this.issueBookButton.TabIndex = 32;
+            this.issueBookButton.Text = "Issue book";
+            this.issueBookButton.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(81, 190);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 39;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(81, 216);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 40;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(974, 409);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.issueBookButton);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.booksListBox);
             this.Controls.Add(this.readersListBox);
@@ -247,6 +300,11 @@
         private System.Windows.Forms.ListBox readersListBox;
         private System.Windows.Forms.ListBox booksListBox;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button issueBookButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
